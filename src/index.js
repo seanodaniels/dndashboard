7a80f19c9608de8002e8814635d0d903bf11777e
Sean ODaniels <sean@Imp.local>
2018-01-31 15:51:14 -0500

    Removed router and refactored code

--- src/index.js
+++ src/index.js
@@ -1,33 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 import './App.css';
 
 // Components
-import App from './components/App.jsx';
-import Spells from './components/Spells/Spells.jsx';
-import Monsters from './components/Monsters/Monsters.jsx';
+import Main from './components/Main.jsx';
 
 // React-Router dependencies
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
-import store, { history } from './store';
-
-const router = (
-  <Provider store={store}>
-    <Router history={history}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Monsters}></IndexRoute>
-        <Route component={Monsters} />
-        <Route component={Spells} />
-      </Route>
-    </Router>
-  </Provider>
-)
-
-// render(router, document.getElementById('root'));
+import store from './store';
 
 ReactDOM.render(
-  router, document.getElementById('root'));
+  <Provider store={store}>
+    <Main />
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
