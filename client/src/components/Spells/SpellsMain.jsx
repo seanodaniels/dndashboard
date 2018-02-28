41856a289e8f9bd6c28dd6c49d72ec9d2f40ca18
Sean ODaniels <sean@Imp.local>
2018-02-28 14:30:56 -0500

    Restructured to use the full base Express package

--- client/src/components/Spells/SpellsMain.jsx
+++ client/src/components/Spells/SpellsMain.jsx
@@ -4,14 +4,27 @@ class SpellsMain extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
   }
 
   render() {
     // DnDashboard
-    return (<div id="spells">
-      Spells
-    </div>)
+    return (
+      <div id="spells">
+        <h2>Spells</h2>
+        <p>{this.state.users.map((user)  => (
+          <div key={user.id}>{user.username}</div>
+        ))}</p>
+      </div>
+    )
   }
 }
 
