029e06345d731f6bdf5e0cfe324db6989323d5ef
Sean ODaniels <sean@Imp.local>
2018-03-23 10:33:58 -0400

    Added filter to Spells list.

--- client/src/components/Spells/SpellsMain.jsx
+++ client/src/components/Spells/SpellsMain.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { spellsFetchData } from "../../actions/index.js";
 
+var _ = require('lodash');
+
 const mapStateToProps = state => {
   return {
     spells: state.spells,
@@ -19,6 +21,24 @@ const mapDispatchToProps = dispatch => {
 class SpellsMain extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      animals: [
+        {
+          animal: 'Heron, giant'
+        }, {
+          animal: 'Lizard'
+        }, {
+          animal: 'Blesbok'
+        }, {
+          animal: 'Ibex'
+        }, {
+          animal: 'Pigeon, wood'
+        }, {
+          animal: 'Ground monitor'
+        }
+      ],
+      query: false
+    }
   }
 
   componentDidMount() {
@@ -31,11 +51,29 @@ class SpellsMain extends Component {
       <div id="spells">
         <h2>Spells</h2>
 
-        {this.props.spells.map((el, index) => (
+        <input placeholder="search" value={this.state.query || ''} onChange={ (event) => { this.setState( { query: event.target.value})}} />
+
+        <p>Query: {this.state.query}</p>
+
+        {/* {this.props.spells.map((el, index) => (
           <li className="list-group-item" key={index}>
             <a href="#">{el.Title}</a>
           </li>
-        ))}
+        ))} */}
+
+        {
+          this.props.spells
+          .filter( 
+            (spell) => _.toLower(spell.Title)
+            .includes(_.toLower(this.state.query || '')))
+            .map((spell, index) => (
+            <li className="list-group-item" key={index}>
+              <a href="">{spell.Title}</a>
+            </li>
+            )
+          )
+        }
+
       </div>
 
     );
