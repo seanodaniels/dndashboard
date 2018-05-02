cc81ec7572aab78815979c8c569ff7181f5fb1d5
Sean ODaniels <sean@Imp.local>
2018-05-02 10:27:51 -0400

    changed stray port 3001 call to port 5000

--- client/src/components/Spells/SpellsList.jsx
+++ client/src/components/Spells/SpellsList.jsx
@@ -29,7 +29,7 @@ class SpellsList extends Component {
   }
 
   componentDidMount() {
-    this.props.fetchSpellsData("http://localhost:3001/api/spells");
+    this.props.fetchSpellsData("http://localhost:5000/api/spells");
   }
 
   handlerChooseSpell(id) {
