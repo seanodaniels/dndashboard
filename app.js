a0d385d3cc25f643dd77a457e6478414996bdb9a
Sean ODaniels <sean@Imp.local>
2018-05-02 12:49:07 -0400

    installed dotenv and configured local environment variable for local MongoDB server use

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var router = express.Router();
+require('dotenv').config();
 
 var index = require("./routes/index");
 var spells = require("./routes/spells");
--- routes/spells.js
+++ routes/spells.js
@@ -6,7 +6,7 @@ const MongoClient = require("mongodb").MongoClient;
 router.get("/", (req, res, next) => {
   var db;
 
-  MongoClient.connect(process.env.MONGODB_DNDASH_URI, (err, client) => {
+  MongoClient.connect(process.env.REACT_APP_MONGODB_DNDASH_URI, (err, client) => {
   // MongoClient.connect("mongodb://localhost:27017", (err, client) => {
     if (err) return console.log(err);
     // db = client.db("mydndb");
