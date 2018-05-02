23149e15b21a01a110fdc43ba188c3a50fd9a71f
Sean ODaniels <sean@Imp.local>
2018-05-02 11:02:59 -0400

    fixed incorrect db call in express

--- routes/spells.js
+++ routes/spells.js
@@ -9,7 +9,8 @@ router.get("/", (req, res, next) => {
   MongoClient.connect("mongodb://dndashboard:mAM4JUNmvtQ2@ds163769.mlab.com:63769/heroku_p9db9w4j", (err, client) => {
   //MongoClient.connect("mongodb://localhost:27017", (err, client) => {
     if (err) return console.log(err);
-    db = client.db("mydndb");
+    // db = client.db("mydndb");
+    db = client.db("heroku_p9db9w4j");
     db
       .collection("spells")
       .find()
